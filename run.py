d94f0febf202c6768a82e486f2c4b0796c5de300
alfiyansys <alfiyan.syamsuddin@gmail.com>
2020-03-27 21:22:43 +0700

    lanjutkan di server aja

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
-import csv
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
 
+series = read_csv('data/indonesia.csv', header=0, parse_dates=[0])
+
+print(series)
